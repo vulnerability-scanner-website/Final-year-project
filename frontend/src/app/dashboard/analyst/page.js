516113788c0ea35ce9e930d7e933f3b4a5c60db4
Alphaguyasa <alphaguyasa@gmail.com>
2026-02-23 15:29:57 +0300

    Create some folder structure

---
+++ frontend/src/app/dashboard/analyst/page.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+const page = () => {
+  return (
+    <div>
+      <h1>Analyst Dashboard</h1>
+      <p>
+        Welcome to the Analyst Dashboard. Here you can view and analyze data
+        related to your projects.
+      </p>
+    </div>
+  );
+};
+
+export default page;
