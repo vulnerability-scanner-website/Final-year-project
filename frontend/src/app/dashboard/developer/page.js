3bbe0de271e63d714d1efdec73f422b48e74d555
ABDIFEKEDA <abdifk23@gmail.com>
2026-02-23 10:56:37 -0800

    add developer sidebar

---
+++ frontend/src/app/dashboard/developer/page.js
@@ -0,0 +1,16 @@
+
+import React from "react";
+
+const page = () => {
+  return (
+    <div>
+      <h1>Developer Dashboard</h1>
+      <p>
+        Welcome to the Developer Dashboard. Here you can view and analyze data
+        related to your projects.
+      </p>
+    </div>
+  );
+};
+
+export default page;
