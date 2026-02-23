f584ea95a0ce808b92d85f45f7c0f354bff08d92
ABDIFEKEDA <abdifk23@gmail.com>
2026-02-23 10:33:44 -0800

    add admin side bar

---
+++ frontend/src/app/dashboard/admin/layout.js
@@ -0,0 +1,13 @@
+import AdminSideBar from "@/components/sidebar/AdminSideBar/Admin.js";
+
+export default function AdminLayout({ children }) {
+  return (
+    <div className="flex min-h-screen">
+      <AdminSideBar />
+
+      <main className="flex-1 p-10 bg-gray-100">
+        {children}
+      </main>
+    </div>
+  );
+}
