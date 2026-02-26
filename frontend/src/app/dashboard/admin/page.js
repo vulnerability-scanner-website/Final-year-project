f0b4fe0582603236b965aa1ede630ad436d8f06d
Alphaguyasa <alphaguyasa@gmail.com>
2026-02-26 10:43:13 +0300

    add cards below the header for admin

--- frontend/src/app/dashboard/admin/page.js
+++ frontend/src/app/dashboard/admin/page.js
@@ -4,11 +4,42 @@ import React from "react";
 import { Button } from "@/components/ui/button";
 import { Calendar } from "@/components/ui/calendar";
 import { DashboardHeader } from "@/components/header/header";
+import { ShieldAlert, Bug, CheckCircle, Activity } from "lucide-react";
+import { StatsCard } from "@/components/statscard/statscard";
 
 export default function Page() {
   return (
     <div className="p-5 space-y-4">
       <DashboardHeader role={"admin"} />
+      <div className="grid gap-4 md:grid-cols-2 lg:grid-cols-4 p-6">
+        <StatsCard
+          title="Total Scans"
+          value="128"
+          description="+12% from last month"
+          icon={Activity}
+        />
+
+        <StatsCard
+          title="Critical Issues"
+          value="12"
+          description="Needs immediate attention"
+          icon={ShieldAlert}
+        />
+
+        <StatsCard
+          title="High Severity"
+          value="23"
+          description="Requires review"
+          icon={Bug}
+        />
+
+        <StatsCard
+          title="Resolved"
+          value="89"
+          description="Issues fixed successfully"
+          icon={CheckCircle}
+        />
+      </div>
     </div>
   );
 }
---
+++ frontend/src/components/statscard/statscard.jsx
@@ -0,0 +1,21 @@
+import { Card, CardHeader, CardTitle, CardContent } from "@/components/ui/card";
+
+export function StatsCard({ title, value, description, icon: Icon }) {
+  return (
+    <Card>
+      <CardHeader className="flex flex-row items-center justify-between space-y-0 pb-2">
+        <CardTitle className="text-sm font-medium text-muted-foreground">
+          {title}
+        </CardTitle>
+        {Icon && <Icon className="h-4 w-4 text-muted-foreground" />}
+      </CardHeader>
+
+      <CardContent>
+        <div className="text-2xl font-bold">{value}</div>
+        {description && (
+          <p className="text-xs text-muted-foreground mt-1">{description}</p>
+        )}
+      </CardContent>
+    </Card>
+  );
+}
