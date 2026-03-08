02d5dfd755193060a166c589827b52c33284dfe2
ABDIFEKEDA <abdifk23@gmail.com>
2026-03-08 00:34:15 -0800

    edit some page

---
+++ frontend/src/app/dashboard/developer/price/page.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import { PricingPage } from "@/components/ui/animated-pricing-page";
+
+function Demoone() {
+  return (
+    <div className="App">
+      <PricingPage />
+    </div>
+  );
+}
+
+export default Demoone;
