47ea2f4bcc2fd5fa974b287e5cd3091b8ac1eecb
ABDIFEKEDA <abdifk23@gmail.com>
2026-03-08 04:05:12 -0700

    fix pricing page error

--- frontend/src/app/dashboard/developer/price/page.jsx
+++ frontend/src/app/dashboard/developer/price/page.jsx
@@ -1,4 +1,16 @@
+"use client";
 
+import React from 'react';
+import PricingSection4 from "@/components/ui/pricing-section-4";
+import DeveloperSideBar from "@/components/sidebar/DeveloperSideBar/Developer";
 
-
-
+export default function Page() {
+  return (
+    <>
+      <DeveloperSideBar />
+      <div className="ml-64">
+        <PricingSection4 />
+      </div>
+    </>
+  );
+}
